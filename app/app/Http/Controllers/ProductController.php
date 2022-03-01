efcc10db4c2e3edb660c030d5e4725790db0e72f
Sergey Kuzmin <winterfield91@yandex.ru>
2022-03-01 11:03:00 +0300

    Added models and relations

--- app/app/Http/Controllers/ProductController.php
+++ app/app/Http/Controllers/ProductController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
     public function index() {
+
         return view('product.index');
     }
 }
